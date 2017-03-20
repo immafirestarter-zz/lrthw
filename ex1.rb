0185d251a1386e8e556509e08416c53678e0c48c
Sami <sami7mason@yahoo.co.uk>
2017-03-20 22:28:55 +0000

    ex1

---
+++ ex1.rb
@@ -0,0 +1,15 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "Hey Hey Hey."
+
+#Study Drill 2:
+puts "Hello World!"
+
+#Study Drill 3:
+#puts 'Hello Again' - this is a comment,
+#used for adding information without interrupting the code
