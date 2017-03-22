4d1766b2626ba150b3cb3080e09feb1cf207a93e
Sami <sami7mason@yahoo.co.uk>
2017-03-22 20:06:58 +0000

    ex5

---
+++ ex5.rb
@@ -0,0 +1,20 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74 #in
+weight = 180 #lb
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+height_cm = height * 2.54
+weight_kg = weight * 0.45359237
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+
+puts "He is #{height_cm} centimeters tall and #{weight_kg} kg fat"
