c54c113b30a5335e1cd5cd56a4526945a1286b97
Sami <sami7mason@yahoo.co.uk>
2017-03-22 20:31:07 +0000

    ex7

---
+++ ex7.rb
@@ -0,0 +1,24 @@
+#below 3 lines are just printing strings (2nd line snow is still a string)
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+#puts '.' 10 times
+puts "." * 10
+
+#declaring many single letter string variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#adding the above variables together to make one word. Here using print means the subsequent puts appears on the same line.
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
