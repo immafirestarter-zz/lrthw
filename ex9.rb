4ab12509df1a6b23c100153ea8ac77461a9d5688
Sami <sami7mason@yahoo.co.uk>
2017-03-23 17:46:25 +0000

    ex9

---
+++ ex9.rb
@@ -0,0 +1,14 @@
+# assigning the days of the week as one string to the days variable.
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# same as above for months but with a newline character after each month, meaning they will appear on different lines.
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+# puts statements printing the days on one line, and the months on multiple.
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+# puts statement with sstrange q character at the start.
+puts %q{
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+}
