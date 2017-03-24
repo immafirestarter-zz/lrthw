7b6bac7fa3aafc8d104d52c5d13d77186d7c1620
Sami <sami7mason@yahoo.co.uk>
2017-03-24 21:17:27 +0000

    ex10

---
+++ ex10.rb
@@ -0,0 +1,15 @@
+tabby_cat = """\tI'm tabbed in."""
+persian_cat = "%{I'm split} \n%{on a line.}"
+backslash_cat = "I'm \\ a \\ ca\ft."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+#you might use triple single quotes if the strong contains a triple double quote inside it.
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
