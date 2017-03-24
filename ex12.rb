daebf8e59c408d62fedba28d5f71bd5f533b5d6c
Sami <sami7mason@yahoo.co.uk>
2017-03-24 21:37:19 +0000

    ex12

---
+++ ex12.rb
@@ -0,0 +1,17 @@
+print "Give me a number: "
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
+
+
+puts "How much money converted to change?"
+money = gets.chomp.to_f
+puts money/10
