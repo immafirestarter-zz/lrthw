50816f0d5a303d3c947f60877c334df39146d7c3
Sami <sami7mason@yahoo.co.uk>
2017-03-27 20:23:39 +0100

    ex28

---
+++ ex28.rb
@@ -0,0 +1,17 @@
+# exercise questions:
+# 1. true 2. false 3. false 4. true 5. true 6. true 7. false 8. true 9. false 10. false
+#11. true 12. false 13. true 14. false 15. false 16. false 17. true 18. true 19. false 20. false
+
+#Study Drills:
+#1 ==  >  equal too
+#2 !=  >  not equal too
+#3 >   >  greater than
+#4 <   >  less than
+#5 >=  >  greater-than-equal too
+#6 <=  >  less-than-equal too
+#7 &&  >  and
+#8 ||  >  or
+#9 <=> >  combined comparision operator
+#10 === >  testing equality in a when clause within a case statement
+#11 .eql >  True if receiver and arg are same type (i.e. both strings) and value.
+#12 equal? > True is receiver and arg have same object id.
