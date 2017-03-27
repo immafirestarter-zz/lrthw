5f81b39fdcaf88ce09fa055304de377b561e2731
Sami <sami7mason@yahoo.co.uk>
2017-03-27 22:45:12 +0100

    ex32

---
+++ ex32.rb
@@ -0,0 +1,26 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+the_count.each do |number|
+  puts "This is count #{number}"
+end
+
+fruits.each do |fruit|
+  puts "A fruit of type: #{fruit}"
+end
+
+change.each {|i| puts "I got #{i}" }
+
+elements = []
+
+(0..5).each do |i|
+  puts "adding #{i} to the list."
+  elements << i
+end
+
+elements.each {|i| puts "Element was: #{i}" }
+
+#Study Drill 1:
+#.. range operator includes all number in range specifed.
+#... range operator includes all numbers in range specified barring the last number.
