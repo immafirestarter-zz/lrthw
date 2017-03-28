f8f32bc487ea9503183098d51c33370d6d26bd0c
Sami <sami7mason@yahoo.co.uk>
2017-03-28 18:55:39 +0100

    ex34

---
+++ ex34.rb
@@ -0,0 +1,20 @@
+#study drill 1:
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+
+puts animals[1]
+puts animals[2]
+puts animals[0]
+puts animals[3]
+puts animals[4]
+puts animals[2]
+puts animals[5]
+puts animals[4]
+
+#rest of study drills:
+#months of the years are ordinal
+
+vegetables = ["broccoli", "carrot", "peas", "cauliflower"]
+puts vegetables[2]
+puts vegetables[3]
+puts vegetables[0]
